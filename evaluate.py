62596516e19cc2387a2668b042aa7e5e1fa1962c
Nitish <nitish2k03@gmail.com>
2025-04-16 07:28:13 +0000

    feat: add box prompts to eval

--- evaluate.py
+++ evaluate.py
@@ -50,12 +50,20 @@ def run_inference(sam2_predictor, frames_path, annotation_path):
 	frames_path = 'data/raw/SegSTRONGC'+frames_path.split('SegSTRONGC')[-1]
 	current_annotations = annotations[frames_path]
 
-	n_points = 0
-	for object in tqdm(current_annotations, desc=f"Processing annotations for objects"):
-		n_points += len(current_annotations[object])
-		for annotation in current_annotations[object]:
-			points = np.array([[int(annotation["x"]), int(annotation["y"])]], np.float32)
-			labels = np.array([annotation["label"]], np.int32)
+	first_frame_annotations = current_annotations[str(0)]
+
+	for object in tqdm(first_frame_annotations, desc=f"Processing annotations for objects"):
+		if "bbox" in first_frame_annotations[object]:
+			box = np.array(first_frame_annotations[object]["bbox"]["box"], dtype=np.float32)
+			_, out_obj_ids, out_mask_logits = sam2_predictor.add_new_points_or_box(
+				inference_state = inference_state,
+				frame_idx = 0,
+				obj_id = int(object),
+				box = box
+			)
+		elif "points" in first_frame_annotations[object]:
+			points = np.array(first_frame_annotations[object]["points"], dtype=np.float32)
+			labels = np.array(first_frame_annotations[object]["labels"], dtype=np.int32)
 			_, out_obj_ids, out_mask_logits = sam2_predictor.add_new_points_or_box(
 				inference_state=inference_state,
 				frame_idx=0,
@@ -63,6 +71,8 @@ def run_inference(sam2_predictor, frames_path, annotation_path):
 				points=points,
 				labels=labels,
 			)
+		else:
+			print(f"Error: No points or box found for object {object} in frame 0.")
 				
 	video_segments = {}
 
@@ -113,16 +123,25 @@ def process_video(model, frames_path, sub_dir, annotation_path, is_left):
 
 	return miou, mdsc
 
-def run_eval(model, sub_dir, domain, annotation_path):
+def run_eval(model, sub_dir, domain, point_annotation_path, box_annotation_path):
 	left_video_frames_path = os.path.join(sub_dir, domain, "left")
 	right_video_frames_path = os.path.join(sub_dir, domain, "right")
 
-	left_miou, left_msdc = process_video(model, left_video_frames_path, sub_dir, annotation_path, True)
-	right_miou, right_msdc = process_video(model, right_video_frames_path, sub_dir, annotation_path, False)
+	point_left_miou, point_left_msdc = process_video(model, left_video_frames_path, sub_dir, point_annotation_path, True)
+	point_right_miou, point_right_msdc = process_video(model, right_video_frames_path, sub_dir, point_annotation_path, False)
 
-	overall_miou = (left_miou + right_miou) / 2
-	overall_msdc = (left_msdc + right_msdc) / 2
-	perf = {"iou": overall_miou, "dsc": overall_msdc}
+	box_left_miou, box_left_msdc = process_video(model, left_video_frames_path, sub_dir, box_annotation_path, True)
+	box_right_miou, box_right_msdc = process_video(model, right_video_frames_path, sub_dir, box_annotation_path, False)
+
+	point_overall_miou = (point_left_miou + point_right_miou) / 2
+	point_overall_msdc = (point_left_msdc + point_right_msdc) / 2
+
+	box_overall_miou = (box_left_miou + box_right_miou) / 2
+	box_overall_msdc = (box_left_msdc + box_right_msdc) / 2
+
+	perf = {"box-iou": box_overall_miou, "box-dsc": box_overall_msdc,
+			"point-iou": point_overall_miou, "point-dsc": point_overall_msdc}
+
 	return perf
 	# print(f"\nResults for {sub_dir}:")
 	# print(f"  Left: IoU={left_miou:.4f}, DSC={left_msdc:.4f}")
@@ -131,7 +150,7 @@ def run_eval(model, sub_dir, domain, annotation_path):
 			
 		
 if __name__ == "__main__":
-	dir = "../segstrong/SegSTRONGC_test/test/9/0/"
+	dir = "data/raw/SegSTRONGC_test/test/9/0/"
 	domain = "blood"
 	DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 	CHECKPOINT = "checkpoints/sam2.1_hiera_base_plus.pt"
@@ -140,4 +159,7 @@ if __name__ == "__main__":
 	torch.autocast("cuda", dtype=torch.bfloat16)
 	annotation_path = "annotations/auto/test.json"
 
-	run_eval(model, dir, domain, annotation_path)
+	point_annotation_path = 'data/prompts/auto/point/1-pos_0-neg/test.json'
+	box_annotation_path = "data/prompts/auto/box/groundingdino/test.json"
+
+	run_eval(model, dir, domain, point_annotation_path, box_annotation_path)
